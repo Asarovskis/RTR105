3b5762c7ce6e30df869ed4fba945e3add4d7862f
Asarovskis <43142487+Asarovskis@users.noreply.github.com>
2018-12-29 16:27:46 +0200

    Update funkcijaszim.py

--- funkcijaszim.py
+++ funkcijaszim.py
@@ -1,6 +1,9 @@
+# Artūrs Asars-Asarovskis Rebco 1 Laboratorijas darbs No2. - Zīmēšana
+#Improtē kāda faili nepieciešami, lai izveidotu grafiku
 import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 from numpy import exp, linspace 
+#Piedefinē robežas, kā arī ievada, kāda funkcija tiek uzzī mēta
 x = linspace(0, 7, 70)
 y= (1-x)*exp(-x)
 
@@ -8,7 +11,9 @@ from matplotlib import pyplot as plt
 plt.grid()
 plt.xlabel('x')
 plt.ylabel('expx')
+#Piedefinē kā sauc doto grafiku
 plt.title('Eksponenciala funkcija')
 plt.plot(x, y)
+#Piedefinē funkcijas līknes krāsu
 plt.plot(x, y, color = "#001100")
 plt.show()
